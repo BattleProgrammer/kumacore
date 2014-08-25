e5d19a5f6bc14d0615e7f73afdcaff696b62ba45
you21979 <you2197901@gmail.com>
2014-08-25 18:08:59 +0900

    set genesis block and bit -> kuma

--- networks.js
+++ networks.js
@@ -4,31 +4,27 @@ var hex = function(hex) {return new Buffer(hex, 'hex');};
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
-  privKeyVersion: 128,
+  magic: hex('dec13500'),
+  addressVersion: 45,//0x2d
+  privKeyVersion: 173,//0x2d+0x80
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('C59A56EEA1B888DE258BD7E757E35040C21867AAE28776779B348D1F844AC201'),//
+    merkle_root: hex('B84509B25957894608B2E115015F6C43DBDB5F0AA57376BBA0379378798220B1'),//
     height: 0,
-    nonce: 2083236893,
+    nonce: 12565946,//
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    bits: 486604799,
+    timestamp: 1398678972,//2014-04-28 09:56:12
+    bits: 537919487,//
   },
   dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
+    'kuma.xau.jp',
+    'kuma.mona-dns.info'
   ],
-  defaultClientPort: 8333
+  defaultClientPort: 7586//
 };
 
 exports.testnet = {
